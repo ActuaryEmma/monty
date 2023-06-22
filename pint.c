81fdb4d5525705ae04ef735d0dbfd04d60d9b04a
konditi1 <konditi1@gmail.com>
2023-06-22 13:24:30 -0700

    resolved conflict of commit

--- pint.c
+++ pint.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-<<<<<<< HEAD
 
 /**
  * pint - Prints the value at the top of the stack.
@@ -18,26 +17,6 @@ void pint(stack_t **stack, unsigned int line_number)
  * @line_number: Line number of the current operation.
  * Return: EXIT_SUCCESS if successful, otherwise EXIT_FAILURE.
  */
-=======
-/**
-  * pint - return the top value in the stack
-  * @stack: pointer to the top of the stack
-  * @line_number: the number of a line in a file
-  * Return: void
-  */
-void pint(stack_t **stack, unsigned int line_number)
-{
-	(void) stack;
-	(void) line_number;
-}
-
-/**
-  * _pint - return the top value in the stack
-  * @head: pointer to the top of the stack
-  * @line_number: the number of a line in a file
-  * Return: EXIT_SUCCESS on success else EXIT_FAILURE
-  */
->>>>>>> 36b3a71761a14019ef3d4782f63d397577ff1f63
 int _pint(stack_t **head, unsigned int line_number)
 {
 	int exit_code = EXIT_SUCCESS;
