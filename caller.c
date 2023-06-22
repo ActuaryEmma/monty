d70312efdab28d906eed2b57f0b684a660a7aa92
ActuaryEmma <emmanyakio@gmail.com>
2023-06-22 21:57:11 +0300

    call function create

--- caller.c
+++ caller.c
@@ -1,34 +1,40 @@
 #include "monty.h"
-
-int caller(stack_t **head, char *opcode, char *operand, unsigned int line_number)
+/**
+  * caller - call the function based on opcode provided
+  * @head: pointer to head of the stack
+  * @opcode: pointer to a array string character
+  * @operand: pointer to a array string character
+  * @line_number: number of the line in a file
+  * Return: EXIT_SUCCESS on success else EXIT_FAILURE
+  */
+int caller(stack_t **head, char *opcode, char *operand,
+				unsigned int line_number)
 {
-		int exit_code = EXIT_SUCCESS;
+	int exit_code = EXIT_SUCCESS;
 
-		if (strcmp(opcode, "push") == 0)
-		{
-				exit_code = _push(head, line_number, operand);
-		}
-		else if (strcmp(opcode, "pall") == 0)
-		{
-				exit_code = _pall(head);
-		}
-		else if (strcmp(opcode, "pint") == 0)
-		{
-				exit_code = _pint(head, line_number);
-		}
-		else if (strcmp(opcode, "pop") == 0)
-		{
-				exit_code = _pop(head, line_number);
-		}
-		else if (strcmp(opcode, "swap") == 0)
-		{
-				exit_code = _swap(head, line_number);
-		}
-		else if (strcmp(opcode, "add_two") == 0)
-		{
-				exit_code = _add_two(head, line_number);
-		}
-
-		return (exit_code);
+	if (strcmp(opcode, "push") == 0)
+	{
+		exit_code = _push(head, line_number, operand);
+	}
+	else if (strcmp(opcode, "pall") == 0)
+	{
+		exit_code = _pall(head);
+	}
+	else if (strcmp(opcode, "pint") == 0)
+	{
+		exit_code = _pint(head, line_number);
+	}
+	else if (strcmp(opcode, "pop") == 0)
+	{
+		exit_code = _pop(head, line_number);
+	}
+	else if (strcmp(opcode, "swap") == 0)
+	{
+		exit_code = _swap(head, line_number);
+	}
+	else if (strcmp(opcode, "add_two") == 0)
+	{
+		exit_code = _add_two(head, line_number);
+	}
+	return (exit_code);
 }
-
