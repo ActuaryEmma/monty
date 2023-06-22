7b36f36c974a52358ad6f26ac9f5acad93c5564d
konditi1 <konditi1@gmail.com>
2023-06-22 13:04:37 -0700

    added the files pall.c parse_line.c pint.c pop.c push.c and swap.c

--- parse_line.c
+++ parse_line.c
@@ -1,40 +1,48 @@
 #include "monty.h"
 
+/**
+ * parse_line - Pases line of Monty bytecode & exect corresponding instruction.
+ * @head: Double pointer to the stack's head.
+ * @line: Line of Monty bytecode.
+ * @line_number: Line number of the current bytecode instruction.
+ * Return: Exit code of the execution.
+ */
 int parse_line(stack_t **head, char *line, unsigned int line_number)
 {
-		char *opcode = NULL, *operand = NULL;
-		int exit_code = EXIT_SUCCESS, index = 0;
+	char *opcode = NULL, *operand = NULL;
+	int exit_code = EXIT_SUCCESS, index = 0;
 
-		instruction_t instructions[] = {
-				{"push", push},
-				{"pall", pall},
-				{"pint", pint},
-				{"pop", pop},
-				{"swap", swap},
-				{"add_two", add_two},
-				{NULL, NULL}
-		};
-		opcode = strtok(line, DELIM);
-		operand = strtok(NULL, DELIM);
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add_two", add_two},
+		{NULL, NULL}
+	};
 
-		if (opcode != NULL)
+	opcode = strtok(line, DELIM);
+	operand = strtok(NULL, DELIM);
+
+	if (opcode != NULL)
+	{
+		for (; instructions[index].opcode != NULL; index++)
 		{
-				for (; instructions[index].opcode != NULL; index++)
-				{
-						if (strcmp(instructions[index].opcode, opcode) == 0)
-						{
-								instructions[index].f(head, line_number);
-								exit_code = caller(head, opcode, operand, line_number);
-								break;
-						}
-				}
-				if (instructions[index].opcode == NULL)
-				{
-						fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-						exit_code = EXIT_FAILURE;
-				}
+			if (strcmp(instructions[index].opcode, opcode) == 0)
+			{
+				instructions[index].f(head, line_number);
+				exit_code = caller(head, opcode, operand, line_number);
+				break;
+			}
 		}
-		
 
-		return (exit_code);
+		if (instructions[index].opcode == NULL)
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+			exit_code = EXIT_FAILURE;
+		}
+	}
+
+	return (exit_code);
 }
--- pint.c
+++ pint.c
@@ -1,18 +1,31 @@
 #include "monty.h"
+
+/**
+ * pint - Prints the value at the top of the stack.
+ * @stack: Double pointer to the stack's head.
+ * @line_number: Line number of the current operation.
+ */
 void pint(stack_t **stack, unsigned int line_number)
 {
-		(void) stack;
-		(void) line_number;
+	(void)stack;
+	(void)line_number;
 }
+
+/**
+ * _pint - Prints the value at the top of the stack.
+ * @head: Double pointer to the stack's head.
+ * @line_number: Line number of the current operation.
+ * Return: EXIT_SUCCESS if successful, otherwise EXIT_FAILURE.
+ */
 int _pint(stack_t **head, unsigned int line_number)
 {
-		int exit_code = EXIT_SUCCESS;
+	int exit_code = EXIT_SUCCESS;
 
-		if (*head == NULL)
-		{
-				fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-				exit_code = EXIT_FAILURE;
-		}
-		printf("%d\n", (*head)->n);
-		return (exit_code);
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit_code = EXIT_FAILURE;
+	}
+	printf("%d\n", (*head)->n);
+	return (exit_code);
 }
