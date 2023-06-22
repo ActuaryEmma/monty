4f61c7684c020f10c95d70e2c19690d5d6e449d0
konditi1 <konditi1@gmail.com>
2023-06-22 13:44:20 -0700

    nop function does nothing and returns void

---
+++ nop.c
@@ -0,0 +1,14 @@
+/**
+ * nop - Does nothing
+ * @stack: Pointer to the head of the stack
+ * @line_number: The line number of the instruction
+ *
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	/* This function does nothing, as specified */
+	(void)stack;
+	(void)line_number;
+	/* Do nothing */
+}
+
