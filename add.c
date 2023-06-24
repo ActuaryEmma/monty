64146268c9634925564f52c544b1599500e58a52
ActuaryEmma <emmanyakio@gmail.com>
2023-06-24 12:10:06 +0300

    betty

--- add.c
+++ add.c
@@ -1,38 +1,48 @@
 #include "monty.h"
-
+/**
+  * add_two - add two digits
+  * @stack: pointer to the head of the list
+  * @line_number: pointer
+  * Return: EXIT SUCCESS else EXIT_FAILURE
+  */
 void add_two(stack_t **stack, unsigned int line_number)
 {
-		(void) stack;
-		(void) line_number;
+	(void) stack;
+	(void) line_number;
 }
+/**
+  * _add_two - add two digits
+  * @head: pointer to the head of the list
+  * @line_number: pointer
+  * Return: EXIT SUCCESS else EXIT_FAILURE
+  */
 int _add_two(stack_t **head, unsigned int line_number)
 {
-    stack_t *temp = NULL;
-    int count = 0;
-    int exit_code = EXIT_SUCCESS;
-    int element1 = 0, element2 = 0;
+	stack_t *temp = NULL;
+	int count = 0;
+	int exit_code = EXIT_SUCCESS;
+	int element1 = 0, element2 = 0;
 
-    if (*head == NULL)
-    {
-        fprintf(stderr, "L%d: can't add, stack too short", line_number);
-        exit_code = EXIT_FAILURE;
-    }
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short", line_number);
+		exit_code = EXIT_FAILURE;
+	}
 
-    temp = *head;
-    while (temp != NULL)
-    {
-        count++;
-        temp = temp->next;
-    }
-
-    if (count < 2)
-    {
-        fprintf(stderr, "L%d: can't add, stack too short", line_number);
-        exit_code = EXIT_FAILURE;
-    }
-    element1 = (*head)->n;
-    element2 = (*head)->next->n;
-    element2 = element1 + element2;
+	temp = *head;
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	if (count < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short", line_number);
+		exit_code = EXIT_FAILURE;
+	}
+	element1 = (*head)->n;
+	element2 = (*head)->next->n;
+	element2 = element1 + element2;
 	(*head)->next->n = element2;
 	(*head) = (*head)->next;
 	free((*head)->prev);
