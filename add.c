a2436d34811108d136f339343629d82058fa77d3
ActuaryEmma <emmanyakio@gmail.com>
2023-06-24 14:41:21 +0300

    updated the add function

--- add.c
+++ add.c
@@ -18,28 +18,15 @@ void add_two(stack_t **stack, unsigned int line_number)
   */
 int _add_two(stack_t **head, unsigned int line_number)
 {
-	stack_t *temp = NULL;
-	int count = 0;
 	int exit_code = EXIT_SUCCESS;
 	int element1 = 0, element2 = 0;
 
-	if (*head == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short", line_number);
-		exit_code = EXIT_FAILURE;
+		return (EXIT_FAILURE);
 	}
 
-	temp = *head;
-	while (temp != NULL)
-	{
-		count++;
-		temp = temp->next;
-	}
-	if (count < 2)
-	{
-		fprintf(stderr, "L%d: can't add, stack too short", line_number);
-		exit_code = EXIT_FAILURE;
-	}
 	element1 = (*head)->n;
 	element2 = (*head)->next->n;
 	element2 = element1 + element2;
